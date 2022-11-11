2ed330008eaeb513d73d6346cfe574cdd2b5838a
JustSharingSomeCode <62771039+JustSharingSomeCode@users.noreply.github.com>
2022-11-11 14:55:36 -0500

    icons component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { ProjectsComponent } from './projects/projects.component';
+import { DeviconComponent } from './devicon/devicon.component';
+import { DeviconContainerComponent } from './devicon-container/devicon-container.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutComponent,
-    ProjectsComponent
+    ProjectsComponent,
+    DeviconComponent,
+    DeviconContainerComponent
   ],
   imports: [
     BrowserModule,
